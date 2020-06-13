4988f4550e95e56cb01b6850aaf2fbc54e5377ec
Cyberego <cy8erego@yandex.ru>
2020-06-13 15:36:43 +0700

    Implemented multiprocessing, changed output format

--- proxy_checker/proxy_checker.py
+++ proxy_checker/proxy_checker.py
@@ -1,27 +1,48 @@
 import requests
 from requests.exceptions import ProxyError, ConnectTimeout
 from pprint import pprint
+from multiprocessing import Process, Manager
+import os
 
 
 TIMEOUT = 3 
 
 
-with open("to_check.txt") as f:
-    proxies = [line.strip() for line in f.readlines()]
-
-good = []
-
-for i, proxy in enumerate(proxies, 1):
-    print(f"{i}/{len(proxies)}")
-
+def check_proxy(proxy: str, list_) -> None:
+    print(f"[{os.getpid()}] {proxy}")
+    proxy_url = f"http://{proxy}"
     try:
-        proxy_url = f"http://{proxy}"
         requests.get("https://httpbin.org/ip", proxies={"http": proxy_url, "https": proxy_url}, timeout=TIMEOUT)
-        good.append(proxy)
+        list_.append(proxy)
     except (ProxyError, ConnectTimeout):
         pass
 
+
+print("=" * 10, "CHECKING", "=" * 10)
+
+# read proxies
+with open("to_check.txt") as f:
+    proxies = [line.strip() for line in f.readlines()]
+
+# check the proxies
+with Manager() as manager:
+    good = manager.list()
+    processes = []
+
+    for proxy in proxies:
+        p = Process(target=check_proxy, args=(proxy, good))
+        processes.append(p)
+        p.start()
+
+    for process in processes:
+        process.join()
+
+    good = list(good)
+
+# write the proxies
 with open("proxies.txt", "w") as f:
     f.writelines([f"{proxy}\n" for proxy in good])
 
-print("Profit!")
+# output the proxies
+print("=" * 10, "PROFIT!", "=" * 10)
+print("\n".join(good))
